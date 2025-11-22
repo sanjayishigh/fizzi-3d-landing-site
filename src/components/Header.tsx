10646eb3e45160053373ddf73fe72c061b6aed1c
sanjayishigh <sanjayishigh@gmail.com>
2025-11-22 11:42:04 +0530

    Changes4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { fizziLogo } from "@/components/FizziLogo";
+import { FizziLogo } from "@/components/FizziLogo";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
     <header className="-mb-28 flex justify-center py-4">
-      <fizziLogo className="z-10 h-20 cursor-pointer text-sky-800" />
+      <FizziLogo className="z-10 h-20 cursor-pointer text-sky-800" />
     </header>
   );
 }
